7eef589e0296752edd26fcd675b3d1067cfcc625
Rishi-Mohan <jharishimohan@gmail.com>
2020-12-16 17:25:03 +0530

    code

---
+++ div3_690/d.cpp
@@ -0,0 +1,20 @@
+/* 
+   author: rishi_mohan
+   created_at: Wed 16 Dec 2020 05:12 PM
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define    ll            long long
+#define    rep(i,a,b)    for(int i=a; i<b; i++)
+#define    all(x)        (x).begin(),(x).end()
+
+void Solve(){}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);  int t = 1;
+	cin >> t;
+	while(t--) Solve();
+}
+
